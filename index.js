02f271b8e2f74d3bea304ba5524631e69dcfcf74
Jacob Terpe Woods <jacobterpewoods@gmail.com>
2024-10-21 13:49:05 +0200

    fix: moved key generation to separate file

--- index.js
+++ index.js
@@ -2,12 +2,10 @@ import express from 'express';
 import router from './routes/index.js';
 import process from 'process';
 import cors from './config/cors.js';
-import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 import { connectToDb } from './db/index.js';
 
 dotenv.config();
-const key = process.env.JWT_KEY;
 const app = express();
 const PORT = process.env.PORT || 8888;
 
--- keyGen.js
+++ keyGen.js
@@ -1,4 +1,14 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+/**
+ * Generate a JWT token for authentication
+ * @returns {string} The generated JWT token
+ */
 function generateKey() {
+	const key = process.env.JWT_KEY;
   const token = jwt.sign(
     {
       user: 'admin',
