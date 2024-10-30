0dd767bad3ce16838fa48304f5b277d1e0c127a5
Jacob Terpe Woods <jacobterpewoods@gmail.com>
2024-10-30 12:34:02 +0100

    fix: added polling stations to testPopulation

--- __test__/db/testPopulation.js
+++ __test__/db/testPopulation.js
@@ -2,8 +2,9 @@ import Party from '../../schemas/Party.js';
 import Constituency from '../../schemas/Constituency.js';
 import NominationDistrict from '../../schemas/NominationDistrict.js';
 import Candidate from '../../schemas/Candidate.js';
+import PollingStation from '../../schemas/PollingStation.js';
 import mockData from './mockData';
-import { districtsWithIds, candidateWithIds } from './addIds';
+import { districtsWithIds, candidateWithIds, pollingStationWithIds } from './addIds';
 
 /**
  * Populate the database with mock data
@@ -13,10 +14,12 @@ export default async function populateDb() {
 	await Constituency.deleteMany({});
 	await NominationDistrict.deleteMany({});
 	await Candidate.deleteMany({});
+	await PollingStation.deleteMany({});
 	await Party.insertMany(mockData.parties);
 	await Constituency.insertMany(mockData.constituencies);
 	await NominationDistrict.insertMany(await districtsWithIds(mockData.nominationDistricts));
 	await Candidate.insertMany(await candidateWithIds(mockData.candidates));
+	await PollingStation.insertMany(await pollingStationWithIds(mockData.pollingStations));
 
 	return;
 }
