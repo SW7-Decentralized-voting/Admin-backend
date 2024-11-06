bb6748cc9f98d1d389a4f69673da0a46e2736c50
Jacob Terpe Woods <jacobterpewoods@gmail.com>
2024-11-06 11:49:36 +0100

    feat: election route now saves privateKey and send publicKey to blockchain

--- controllers/election.js
+++ controllers/election.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 import * as e from 'express';
 import { getAllCandidates } from '../utils/candidateHelpers.js';
 import { getAllParties } from '../utils/partyHelpers.js';
+import { getKeyPair } from '../utils/encryptionKeys.js';
+import KeyPair from '../schemas/KeyPair.js';
 
 dotenv.config();
 const url = process.env.BLOCKCHAIN_URL + '/election';
@@ -13,24 +15,36 @@ const url = process.env.BLOCKCHAIN_URL + '/election';
  * @returns {e.Response} Success or error message
  */
 async function startElection(res) {
-  let candidates, parties;
+  let candidates, parties, keyPair;
   try {
     candidates = await getAllCandidates();
     parties = await getAllParties();
+    keyPair = await getKeyPair();
+    KeyPair({ publicKey: keyPair.publicKey, privateKey: keyPair.privateKey }).save();
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
+
+  console.log({
+    candidates: candidates,
+    parties: parties,
+    publicKey: keyPair.publicKey
+  });
 
   return await axios.post(`${url}/start`, {
     // Add body with all candidates and parties
     candidates: candidates,
     parties: parties,
+    publicKey: keyPair.publicKey
   })
     .then(response => {
       return res.status(200).json(response.data);
     })
     .catch(error => {
-      if (error.response.status === 404) {
+      if (!error.response) {
+        return res.status(500).json({ error: 'Blockchain service cannot be reached' });
+      }
+      if (error.response?.status === 404) {
         return res.status(500).json({ error: 'Blockchain service cannot be reached' });
       }
       return res.status(error.response.status).json(error.response.data);
