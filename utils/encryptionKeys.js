d45b3d4baebad8dd9ae54e491647e3223c2995f3
Fantaskink <45458221+Fantaskink@users.noreply.github.com>
2024-12-05 14:10:42 +0100

    fix: linter warning

--- utils/encryptionKeys.js
+++ utils/encryptionKeys.js
@@ -2,7 +2,7 @@ import * as paillierBigint from 'paillier-bigint';
 
 /**
  * 
- * @returns {Object} A key pair for the Paillier cryptosystem
+ * @returns {object} A key pair for the Paillier cryptosystem
  */
 async function getKeyPair() {
 	return paillierBigint.generateRandomKeys(2048);
