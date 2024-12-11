6bc5271e943dcf647c87b818ffc0742942f27eda
MortenMunk <mortenmunk97@gmail.com>
2024-12-11 13:38:02 +0100

    hotfix: tally

--- controllers/tally.js
+++ controllers/tally.js
@@ -13,14 +13,22 @@ const url = process.env.BLOCKCHAIN_URL + '/tally';
 async function tallyVotes(res) {
     // Get latest private key from database
     try {
-        const privateKey = await KeyPair.findOne().sort({ _id: -1 });
-        if (!privateKey) {
+        const mongoKey = await KeyPair.findOne().sort({ _id: -1 });
+        if (!mongoKey) {
             return res.status(404).json({
                 error: 'No private key found',
             });
         }
+        const publicKey = mongoKey.publicKey;
+        const privateKey = {
+            lambda: mongoKey.lambda,
+            mu: mongoKey.mu,
+            publicKey: publicKey
+        };
+        // eslint-disable-next-line no-console
+        console.log(mongoKey);
         const response = await axios.post(url, {
-            privateKey: privateKey.privateKey,
+            privateKey: privateKey,
         });
         return res.status(200).json({
             message: 'Votes tallied successfully',
@@ -31,6 +39,7 @@ async function tallyVotes(res) {
         console.error(`Error fetching private key: ${error.message}`);
         return res.status(500).json({
             error: 'An unexpected error occurred while fetching the private key',
+            message: error.message,
         });
     }
 }
