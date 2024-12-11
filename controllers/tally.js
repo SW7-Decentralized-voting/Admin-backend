2d221438807d7e9f98e88d23641890248e652c28
MortenMunk <mortenmunk97@gmail.com>
2024-12-11 13:03:52 +0100

    hotfix: tally functionality

--- controllers/tally.js
+++ controllers/tally.js
@@ -1,19 +1,38 @@
 import dotenv from 'dotenv';
 import axios from 'axios';
-import * as e from 'express';
-import { getAllCandidates } from '../utils/candidateHelpers.js';
-import { getAllParties } from '../utils/partyHelpers.js';
-import { getKeyPair } from '../utils/encryptionKeys.js';
 import KeyPair from '../schemas/KeyPair.js';
 
 dotenv.config();
 const url = process.env.BLOCKCHAIN_URL + '/tally';
 
+/**
+ * Tally votes by posting the latest private key to the blockchain.
+ * @param {object} res - The response object.
+ * @returns {Promise<void>} - A promise that resolves when the operation is complete.
+ */
 async function tallyVotes(res) {
-    // Get private key from database
-    // Post privateKey as body to blockchain
-    let privateKey;
-    
+    // Get latest private key from database
+    try {
+        const privateKey = await KeyPair.findOne().sort({ _id: -1 });
+        if (!privateKey) {
+            return res.status(404).json({
+                error: 'No private key found',
+            });
+        }
+        const response = axios.post(url, {
+            privateKey: privateKey.privateKey,
+        });
+        return res.status(200).json({
+            message: 'Votes tallied successfully',
+            tally: response.data,
+        });
+    } catch (error) {
+        // eslint-disable-next-line no-console
+        console.error(`Error fetching private key: ${error.message}`);
+        return res.status(500).json({
+            error: 'An unexpected error occurred while fetching the private key',
+        });
+    }
 }
 
 export { tallyVotes };
